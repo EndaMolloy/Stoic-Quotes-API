e2dc23f44a99aff11891d06da7d7793d1ebaf05f
EndaMolloy <enda.molloy.1@gmail.com>
2018-02-20 13:50:45 +0000

    add CORS

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ mongoose.connect(mongoDB);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/quotes/:dayNum', async (req,res)=> {
   const dayNum = req.params.dayNum;
   Quote.find({},(err,quotes)=> {
